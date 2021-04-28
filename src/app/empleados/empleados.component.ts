4cec4c20890e4ee87817d1ee8bd73bb03eb3772c
daniel juan <danieleduard1994@gmail.com>
2021-04-27 21:02:11 -0700

    iniciando curso angular

---
+++ src/app/empleados/empleados.component.ts
@@ -0,0 +1,11 @@
+import { style } from "@angular/animations";
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-empleados",
+    templateUrl: "./empleados.component.html",
+    styleUrls: ["./empleados.component.css"]
+})
+export class EmpleadosComponent{
+
+}
